9df0216d1c4cf101066593cec3fbcb1c1eefb1c8
clarmso <clare.m.so@gmail.com>
2015-06-28 01:43:03 -0400

    Working copy.  Ready for modal.

--- GuessCountry.js
+++ GuessCountry.js
@@ -1,47 +1,64 @@
 var loc = [
-	{lat: 51.064158, lng: -114.071241, zoom: 16},
-	{lat: 42.344866, lng: -71.071247, zoom: 16},
-	{lat: 43.450418, lng: -80.497517, zoom: 16},
-	{lat: 51.515332, lng: -0.122399, zoom: 16},
-	{lat: 48.861715, lng: 2.336221, zoom: 16},
-	{lat: 52.367915, lng: 4.900242, zoom: 16},
-	{lat: 4.7312924, lng: -74.1046165, zoom: 16},
-	{lat: -34.586245, lng: -58.466545, zoom: 16},
-	{lat: 47.099953, lng: -56.377437, zoom: 16},
-	{lat: 39.027944, lng: 125.741913, zoom: 15},
-	{lat: 39.0156744, lng: 125.7381794, zoom: 14},
-	{lat: -34.587509, lng: -70.993077, zoom: 16},
-	{lat: 37.5488732, lng: 126.9742271, zoom: 17},
-	{lat: 35.6696131, lng: 139.7749516, zoom: 15},
-	{lat: 1.3014784, lng: 103.8481995, zoom: 16},
-	{lat: -27.4774069, lng: 153.0123612, zoom: 16},
-	{lat: -33.8751475, lng: 151.1659311, zoom: 16},
-	{lat: -37.8122523, lng: 144.9646639, zoom: 15},
-	{lat: 48.1540392, lng: 11.5667795, zoom: 16},
-	{lat: 53.5495689, lng: 9.9884819, zoom: 16},
-	{lat: 51.5139742, lng: 7.4660139, zoom: 17},
-	{lat: 50.8533137, lng: 4.3328504, zoom: 16},
-	{lat: 51.2227676, lng: 4.3976741, zoom: 16},
-	{lat: 50.6391703, lng: 3.0608396, zoom: 16},
-	{lat: 49.9257157, lng: 1.0764878, zoom: 17},
-	{lat: 44.8401805, lng: -0.5706461, zoom: 16},
-	{lat: 40.4208109, lng: -3.7062342, zoom: 17},
-	{lat: 38.7142507, lng: -9.145207, zoom: 16},
-	{lat: 36.8292151, lng: 10.170732, zoom: 16},
-	{lat: 30.072692, lng: 31.2172858, zoom: 16},
-	{lat: 37.9863165, lng: 23.7258117, zoom: 16}, 
-	{lat: 41.9046049, lng: 12.4879997, zoom: 16},
-	{lat: 46.9482812, lng: 7.4502318, zoom: 16},
-	{lat: 46.2016181, lng: 6.1398976, zoom: 16},
-	{lat: 47.4989923, lng: 8.7289159, zoom: 16},
-	{lat: 47.0514957, lng: 8.3055131, zoom: 16},
-	{lat:49.2569332, lng: -123.1239135, zoom: 16},
-	{lat: 19.394392, lng: -99.1669477, zoom: 15},
-	{lat: -34.9188925, lng: -56.1539412, zoom: 16},
-	{lat: 34.5948209, lng: -58.3725539, zoom: 16},
-	{lat: 40.7623722, lng: -111.9160648, zoom: 16},
-	{lat: 48.837791, lng: 2.375084, zoom: 16},
-	{lat: 45.5473133, lng: -73.69393, zoom: 16}
+	{lat:45.500169, lng: -73.565767, zoom: 15},
+	{lat: 53.5378498, lng: -113.4819752, zoom: 15},
+	{lat: 49.167993, lng: -123.938844, zoom: 15},
+	{lat: 37.7577, lng: -122.4376, zoom: 15},
+	{lat: 40.776608, lng: -111.920485, zoom: 15},
+	{lat: 42.315522, lng: -71.071006, zoom: 15},
+	{lat: 41.8718364, lng: -87.6301886, zoom: 15},
+	{lat: 19.3907337, lng: -99.1436126, zoom: 15},
+	{lat: 14.6117275, lng: -90.5371495, zoom: 16},
+	{lat: -34.9107247, lng: -56.1712948, zoom: 16},
+	{lat: -34.6056691, lng: -58.3696339, zoom: 16},
+	{lat: 4.6482976, lng: -74.107807, zoom: 16},
+	{lat: 10.4682651, lng: -66.8818686, zoom: 15},
+	{lat: 23.1303107, lng: -82.3555189, zoom: 16},
+	{lat: 18.4374906, lng: -69.9553341, zoom: 16},
+	{lat: 51.5019619, lng: -0.0176133, zoom: 16},
+	{lat: 51.5084797, lng: -0.1107611, zoom: 16},
+	{lat: 48.854947, lng: 2.348606, zoom: 16},
+	{lat: 48.827775, lng: 2.386844, zoom: 16},
+	{lat: 50.6274877, lng: 3.0609577, zoom: 16},
+	{lat: 43.6007, lng: 1.432841, zoom: 16},
+	{lat: 38.7103644, lng: -9.1365144, zoom: 16},
+	{lat: 46.2050295, lng: 6.1440885, zoom: 16},
+	{lat: 46.9482251, lng: 7.4512394, zoom: 16},
+	{lat: 47.0508076, lng: 8.3063376, zoom: 17},
+	{lat: 53.0713858, lng: 8.8056544, zoom: 16},
+	{lat: 53.5565774, lng: 9.9953496, zoom: 16},
+	{lat: 50.1207167, lng: 8.6630855, zoom: 16},
+	{lat: 48.2266176, lng: 16.4048435, zoom: 16},
+	{lat: 41.9030897, lng: 12.4524904, zoom: 17},
+	{lat: 41.3748912, lng: 2.1787657, zoom: 16},
+	{lat: 41.8909407, lng: 12.4726472, zoom: 17},
+	{lat: 44.4255271, lng: 26.1028893, zoom: 17},
+	{lat: 52.225315, lng: 21.0455299, zoom: 16},
+	{lat: 53.9072295, lng: 27.5551486, zoom: 16},
+	{lat: 55.745028, lng: 37.641378, zoom: 16},
+	{lat: 30.0456701, lng: 31.2293248, zoom: 16},
+	{lat:36.8067726, lng: 10.1798415, zoom: 16},
+	{lat: 33.5912986, lng: -7.6155516, zoom: 16},
+	{lat: -26.2037546, lng: 28.0459898, zoom: 16},
+	{lat: 41.013628, lng: 29.0163637, zoom: 16},
+	{lat: 35.6994628, lng: 51.3418793, zoom: 17},
+	{lat: 22.3576782, lng: 114.1210175, zoom: 16},
+	{lat: 22.1892106, lng: 113.5505443, zoom: 16},
+	{lat: 1.3049593, lng: 103.8585892, zoom: 17},
+	{lat: -43.5053654, lng: 172.7074451, zoom: 16},
+	{lat: -33.8605809, lng: 151.2112077, zoom: 17},
+	{lat: -41.2929963, lng: 174.7830762, zoom: 16},
+	{lat: 35.6827261, lng: 139.7605716, zoom: 16},
+	{lat: 43.069364, lng: 141.355098, zoom: 16},
+	{lat: 31.2201101, lng: 121.4933181, zoom: 16},
+	{lat: 37.5651, lng: 126.98955, zoom: 16},
+	{lat: 14.5980716, lng: 120.9796513, zoom: 17},
+	{lat: 47.9160682, lng: 106.9052617, zoom: 17},
+	{lat: 38.9152736, lng: 121.6090715, zoom: 17},
+	{lat: 35.8332757, lng: 127.1262989, zoom: 17},
+	{lat: 3.1355534, lng: 101.6935288, zoom: 17},
+	{lat: -6.1412583, lng: 106.812266, zoom: 17},
+	{lat: 25.0509905, lng: 121.542395, zoom: 17},
+	{lat: 56.3368248, lng: -2.7898692, zoom: 16}
 ];
 
 var score = 0;
@@ -55,8 +72,8 @@ function initialize() {
 	l = loc[i];
     var mapOptions = {
         center: { lat: l.lat, lng: l.lng},
-		mapTypeId: google.maps.MapTypeId.TERRAIN,		
-        zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROAD,		
+        zoom: l.zoom,
 		disableDefaultUI: true,
 		disableDoubleClickZoom: true,
 		keyboardShortcuts: false,
@@ -84,11 +101,16 @@ function initialize() {
 
 function validateCountry() {
 	var c = document.getElementById("country").value.trim();
+
+	if (c==null || c=="") {
+		alert("Please enter the name of the country.");
+		return false;
+	}
+
 	var latlng = new google.maps.LatLng(l.lat, l.lng);	
 	var addr;
 	var longCountry;
 	var shortCountry;
-	var city;
 	var scoreElement;
 	geocoder.geocode({
 		'latLng': latlng, 
@@ -101,19 +123,17 @@ function validateCountry() {
 					addr = results[0].address_components;
 					longCountry = "";
 					shortCountry = "";
-					hint = "";
+
 					for (i=0; i<addr.length; i++) {
 						if (addr[i].types.indexOf("country")>-1) {
 							longCountry = addr[i].long_name;
 							shortCountry = addr[i].short_name;
 						}
-						if (addr[i].types.indexOf("locality")>-1) {
-							city = addr[i].long_name;
-						}
 					}
 
 					if (longCountry.indexOf(c)>-1  || shortCountry.indexOf(c)>-1) {
 						alert('Congrats! This is in '+longCountry+".");
+						//${answerModal}.modal('show');
 						score = score + 1;
 					}	
 					else {
