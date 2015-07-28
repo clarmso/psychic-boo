64f2b907bf174ef16ee31008acd217cb9b4af11b
clarmso <clare.m.so@gmail.com>
2015-07-28 00:53:59 -0400

    10 questions per game
    Issue #9
    
    In addition, all countries in the world are included in the
    autocomplete.

--- client/GuessCountry.js
+++ client/GuessCountry.js
@@ -66,13 +66,264 @@ var loc = [
 ];
 
 var listOfCountries = [
-	"Argentina", "Australia", "Belarus", "Canada", "China",
-	"Colombia", "France", "Germany", "Italy", "Japan", "Mexico",
-	"New Zealand", "Portugal", "Romania", "Russia", "South Africa",
-	"South Korea", "Spain", "Switzerland", "Turkey", "Vatican City",
-	"United States", "United Kingdom", "Tunisia", "Iran", "Vietnam",
-	"Tanzania", "Ghana", "Brazil", "Chile"
-];
+	"Afghanistan",
+	"Akrotiri",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Ashmore and Cartier Islands",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas, The",
+	"Bahrain",
+	"Bangladesh",
+	"Barbados",
+	"Bassas da India",
+	"Belarus",
+	"Belgium",
+	"Belize",
+	"Benin",
+	"Bermuda",
+	"Bhutan",
+	"Bolivia",
+	"Bosnia and Herzegovina",
+	"Botswana",
+	"Bouvet Island",
+	"Brazil",
+	"British Indian Ocean Territory",
+	"British Virgin Islands",
+	"Brunei",
+	"Bulgaria",
+	"Burkina Faso",
+	"Burma",
+	"Burundi",
+	"Cambodia",
+	"Cameroon",
+	"Canada",
+	"Cape Verde",
+	"Cayman Islands",
+	"Central African Republic",
+	"Chad",
+	"Chile",
+	"China",
+	"Christmas Island",
+	"Clipperton Island",
+	"Cocos (Keeling) Islands",
+	"Colombia",
+	"Comoros",
+	"Congo, Democratic Republic of the",
+	"Congo, Republic of the",
+	"Cook Islands",
+	"Coral Sea Islands",
+	"Costa Rica",
+	"Cote d'Ivoire",
+	"Croatia",
+	"Cuba",
+	"Cyprus",
+	"Czech Republic",
+	"Denmark",
+	"Dhekelia",
+	"Djibouti",
+	"Dominica",
+	"Dominican Republic",
+	"Ecuador",
+	"Egypt",
+	"El Salvador",
+	"Equatorial Guinea",
+	"Eritrea",
+	"Estonia",
+	"Ethiopia",
+	"Europa Island",
+	"Falkland Islands (Islas Malvinas)",
+	"Faroe Islands",
+	"Fiji",
+	"Finland",
+	"France",
+	"French Guiana",
+	"French Polynesia",
+	"French Southern and Antarctic Lands",
+	"Gabon",
+	"Gambia, The",
+	"Gaza Strip",
+	"Georgia",
+	"Germany",
+	"Ghana",
+	"Gibraltar",
+	"Glorioso Islands",
+	"Greece",
+	"Greenland",
+	"Grenada",
+	"Guadeloupe",
+	"Guam",
+	"Guatemala",
+	"Guernsey",
+	"Guinea",
+	"Guinea-Bissau",
+	"Guyana",
+	"Haiti",
+	"Heard Island and McDonald Islands",
+	"Holy See (Vatican City)",
+	"Honduras",
+	"Hong Kong",
+	"Hungary",
+	"Iceland",
+	"India",
+	"Indonesia",
+	"Iran",
+	"Iraq",
+	"Ireland",
+	"Isle of Man",
+	"Israel",
+	"Italy",
+	"Jamaica",
+	"Jan Mayen",
+	"Japan",
+	"Jersey",
+	"Jordan",
+	"Juan de Nova Island",
+	"Kazakhstan",
+	"Kenya",
+	"Kiribati",
+	"Korea, North",
+	"Korea, South",
+	"Kuwait",
+	"Kyrgyzstan",
+	"Laos",
+	"Latvia",
+	"Lebanon",
+	"Lesotho",
+	"Liberia",
+	"Libya",
+	"Liechtenstein",
+	"Lithuania",
+	"Luxembourg",
+	"Macau",
+	"Macedonia",
+	"Madagascar",
+	"Malawi",
+	"Malaysia",
+	"Maldives",
+	"Mali",
+	"Malta",
+	"Marshall Islands",
+	"Martinique",
+	"Mauritania",
+	"Mauritius",
+	"Mayotte",
+	"Mexico",
+	"Micronesia, Federated States of",
+	"Moldova",
+	"Monaco",
+	"Mongolia",
+	"Montserrat",
+	"Morocco",
+	"Mozambique",
+	"Namibia",
+	"Nauru",
+	"Navassa Island",
+	"Nepal",
+	"Netherlands",
+	"Netherlands Antilles",
+	"New Caledonia",
+	"New Zealand",
+	"Nicaragua",
+	"Niger",
+	"Nigeria",
+	"Niue",
+	"Norfolk Island",
+	"Northern Mariana Islands",
+	"Norway",
+	"Oman",
+	"Pakistan",
+	"Palau",
+	"Panama",
+	"Papua New Guinea",
+	"Paracel Islands",
+	"Paraguay",
+	"Peru",
+	"Philippines",
+	"Pitcairn Islands",
+	"Poland",
+	"Portugal",
+	"Puerto Rico",
+	"Qatar",
+	"Reunion",
+	"Romania",
+	"Russia",
+	"Rwanda",
+	"Saint Helena",
+	"Saint Kitts and Nevis",
+	"Saint Lucia",
+	"Saint Pierre and Miquelon",
+	"Saint Vincent and the Grenadines",
+	"Samoa",
+	"San Marino",
+	"Sao Tome and Principe",
+	"Saudi Arabia",
+	"Senegal",
+	"Serbia and Montenegro",
+	"Seychelles",
+	"Sierra Leone",
+	"Singapore",
+	"Slovakia",
+	"Slovenia",
+	"Solomon Islands",
+	"Somalia",
+	"South Africa",
+	"South Georgia and the South Sandwich Islands",
+	"Spain",
+	"Spratly Islands",
+	"Sri Lanka",
+	"Sudan",
+	"Suriname",
+	"Svalbard",
+	"Swaziland",
+	"Sweden",
+	"Switzerland",
+	"Syria",
+	"Taiwan",
+	"Tajikistan",
+	"Tanzania",
+	"Thailand",
+	"Timor-Leste",
+	"Togo",
+	"Tokelau",
+	"Tonga",
+	"Trinidad and Tobago",
+	"Tromelin Island",
+	"Tunisia",
+	"Turkey",
+	"Turkmenistan",
+	"Turks and Caicos Islands",
+	"Tuvalu",
+	"Uganda",
+	"Ukraine",
+	"United Arab Emirates",
+	"United Kingdom",
+	"United States",
+	"Uruguay",
+	"Uzbekistan",
+	"Vanuatu",
+	"Venezuela",
+	"Vietnam",
+	"Virgin Islands",
+	"Wake Island",
+	"Wallis and Futuna",
+	"West Bank",
+	"Western Sahara",
+	"Yemen",
+	"Zambia",
+	"Zimbabwe"
+].sort();
 
 
 var noMeme = [
@@ -108,8 +359,29 @@ var score = 0;
 
 var l;
 var geocoder = new google.maps.Geocoder();
+var numQuestions = 10;
 
 function initialize() {
+	numQuestions--;
+
+	if (numQuestions==0) {
+		$("#answer").text("Your final score: "+score+"/10");
+		$("#answer-div").removeClass("alert alert-danger")
+			.removeClass("alert alert-success")
+			.addClass("alert alert-info");
+
+		if (score>5) {
+			random = Math.trunc(Math.random() * yesMeme.length);
+			$("#meme").attr("src", yesMeme[random]);
+		}	else {
+			random = Math.trunc(Math.random() * noMeme.length);
+			$("#meme").attr("src",noMeme[random]);
+		}
+		$("#nextQuestion").text("New Game");
+		console.log("show final score");
+		$('#myModal').modal('show');
+	}
+
 	var i = Math.trunc(Math.random() * loc.length);
 	l = loc[i];
   var mapOptions = {
@@ -211,15 +483,18 @@ function validateCountry() {
 $(document).ready(function() {
 
 	$('#myModal').on('hidden.bs.modal', function () {
-			initialize();
+			console.log('hide modal');
+			$("#nextQuestion").text("Next Question");
+			if (numQuestions==0) {
+				location.reload();
+			} else {
+				initialize();
+			}
 	});
 
 	$('#myModal').keyup(function(event) {
-		console.log('hey');
-		if (event.keyCode==13) {
-			$('#myModal').modal('hide');
-		}
-		if (event.keyCode==32) {
+		console.log('close modal by enter or space bar');
+		if ( (event.keyCode==13) || (event.keyCode==32) ){
 			$('#myModal').modal('hide');
 		}
 	});
@@ -230,6 +505,7 @@ $(document).ready(function() {
 		$("#enter").prop("disabled", false);
 	});
 	$("#country").keyup(function(event) {
+		console.log('country entered');
 		if ( (event.keyCode==13) && ($("#country").val().trim()!="") ){
 			validateCountry();
 		}
